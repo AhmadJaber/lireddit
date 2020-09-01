e314210204fd835e0a38c710a8e4aec35e2e1149
AhmadJaber <jaber131706@gmail.com>
2020-09-01 17:03:48 +0600

    frontend added, nextjs&chakra template added, registerui completed

---
+++ lireddit-web/src/pages/register.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Button, Box } from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="UserName"
+            />
+
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="Password"
+              />
+            </Box>
+
+            <Box mt={4}>
+              <Button
+                type="submit"
+                isLoading={isSubmitting}
+                variantColor="teal"
+              >
+                Register
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
