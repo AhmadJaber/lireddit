0b464d7a7c721a0cdc858fb415916e4b5beb9a02
AhmadJaber <jaber131706@gmail.com>
2020-09-02 21:59:00 +0600

    login page & error handling added

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -3,24 +3,24 @@ import { Formik, Form } from "formik";
 import { Button, Box } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useRegisterMutation } from "../generated/graphql";
+import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
 
-const Register: React.FC<{}> = ({}) => {
+const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
-  const [, register] = useRegisterMutation();
+  const [, login] = useLoginMutation();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
+          const response = await login({ options: values });
 
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
             router.push("/");
           }
         }}
@@ -48,7 +48,7 @@ const Register: React.FC<{}> = ({}) => {
                 isLoading={isSubmitting}
                 variantColor="teal"
               >
-                Register
+                Login
               </Button>
             </Box>
           </Form>
@@ -58,4 +58,4 @@ const Register: React.FC<{}> = ({}) => {
   );
 };
 
-export default Register;
+export default Login;
