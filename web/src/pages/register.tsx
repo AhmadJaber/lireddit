26ae6da511363ee66b875613ec71d9620d73b114
AhmadJaber <jaber131706@gmail.com>
2020-09-02 13:16:46 +0600

    graphql code gen setup completed, typescript type&hooks generated

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,37 +3,21 @@ import { Formik, Form } from "formik";
 import { Button, Box } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
-const REGISTER_MUTATION = `
-mutation Register($username: String!, $password: String!) {
-    register(options: { username: $username, password: $password }) {
-      errors {
-        field
-        message
-      }
-      user {
-        id
-        createdAt
-        username
-        updatedAt
-      }
-    }
-}
-`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUTATION);
+  const [, register] = useRegisterMutation();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
+        onSubmit={async (values) => {
           console.log(values);
-          return register(values);
+          const response = await register(values);
+          response.data?.register.user?.id;
         }}
       >
         {({ isSubmitting }) => (
