c2f5a6466067b851828fb6c5f233614598e8a342
AhmadJaber <jaber131706@gmail.com>
2020-09-17 16:15:25 +0600

    style posts with chakra & fieldresolvers

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,32 +1,52 @@
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
-import { usePostsQuery } from "../generated/graphql";
+import { useMeQuery, usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
-import { Link } from "@chakra-ui/core";
+import { Link, Stack, Box, Text, Heading, Flex, Button } from "@chakra-ui/core";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
 
+  if (!data && !fetching) {
+    return <div>you got query failed for some reason</div>;
+  }
+
   return (
     <>
       <Layout variant="regular">
-        <NextLink href="/create-post">
-          <Link color="teal.500">Create Post!!</Link>
-        </NextLink>
-
-        <div>Home Page</div>
+        <Flex justifyContent="space-between" alignItems="center">
+          <Heading>LiReddit</Heading>
+          <NextLink href="/create-post">
+            <Link color="teal.500">Create Post!!</Link>
+          </NextLink>
+        </Flex>
 
         <br />
-        {!data ? (
+        {!data && fetching ? (
           <div>Loading....</div>
         ) : (
-          data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+          <Stack spacing={8}>
+            {data!.posts.map((p) => (
+              <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text mt={4}>{`${p.textSnippet} ....`}</Text>
+              </Box>
+            ))}
+          </Stack>
         )}
+
+        {data ? (
+          <Flex>
+            <Button my={8} mx="auto" isLoading={fetching}>
+              Load More
+            </Button>
+          </Flex>
+        ) : null}
       </Layout>
     </>
   );
